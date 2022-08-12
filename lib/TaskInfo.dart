ef11bb91f2502e1fbc0ebc59a3ef1688cddc3a28
konan080211 <konanforbis@gmail.com>
2022-08-12 20:22:35 +0900

    fix: line通知の移管による削除

--- lib/TaskInfo.dart
+++ lib/TaskInfo.dart
@@ -25,9 +25,6 @@ class _TaskInfoState extends State<TaskInfo> {
   String _taskTimeMinutes = '';
   String _taskTimeSeconds = '';
 
-  // 規定通知回数
-  int _lineNotifyNum = 5;
-
   int _taskInfoCllorR = 66;
   int _taskInfoCllorG = 66;
   int _taskInfoCllorB = 66;
@@ -144,33 +141,12 @@ class _TaskInfoState extends State<TaskInfo> {
     }
   }
 
-  Future<void> sendLineMessage() async {
-    String url = 'https://api.line.me/v2/bot/message/push';
-    Map<String, String> headers = {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + dotenv.env['CHANNEL_TOKEN']!
-    };
-    String body = json.encode({
-      "to": "Uf6dc052d62c879a4e6befd2fa43dd742",
-      "messages": [
-        {"type": "text", "text": _taskName + ' 終了です!'}
-      ]
-    });
-    // 規定通知回数を下回ったら通知の停止
-    if (_lineNotifyNum > 0) {
-      await http.post(Uri.parse(url), headers: headers, body: body);
-      _lineNotifyNum--;
-    }
-  }
-
   /*
     アラート条件
     ・NT      : アラート
     ・WORK    : 25分以上でアラート
     ・SBRK    : 5分以上でアラート
     ・LBRK    : 10分以上でアラート
-    ・MTG     : アラートなし
-    ・LUNCH   : 60分以上でアラート
     ・上記以外 : デフォルトカラー
   */
   void manageTaskInfoColor() {
@@ -190,7 +166,6 @@ class _TaskInfoState extends State<TaskInfo> {
           _taskInfoCllorB = 67;
         });
       }
-      sendLineMessage();
     } else if (_taskName == SBRK && _taskTime >= 300) {
       if (mounted) {
         setState(() {
@@ -199,7 +174,6 @@ class _TaskInfoState extends State<TaskInfo> {
           _taskInfoCllorB = 67;
         });
       }
-      sendLineMessage();
     } else if (_taskName == LBRK && _taskTime >= 600) {
       if (mounted) {
         setState(() {
@@ -208,18 +182,6 @@ class _TaskInfoState extends State<TaskInfo> {
           _taskInfoCllorB = 67;
         });
       }
-      sendLineMessage();
-    } else if (_taskName == MTG && _taskTime >= 300) {
-      // アラートなし
-    } else if (_taskName == LUNCH && _taskTime >= 3600) {
-      if (mounted) {
-        setState(() {
-          _taskInfoCllorR = 191;
-          _taskInfoCllorG = 67;
-          _taskInfoCllorB = 67;
-        });
-      }
-      sendLineMessage();
     } else {
       if (mounted) {
         setState(() {
@@ -228,7 +190,6 @@ class _TaskInfoState extends State<TaskInfo> {
           _taskInfoCllorB = 66;
         });
       }
-      _lineNotifyNum = 5;
     }
   }
 }
